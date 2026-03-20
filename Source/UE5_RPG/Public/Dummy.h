ca587c951770d1237ba70b0580d93852a31e4973
Ana Hernández <anaisabel14hb@gmail.com>
2026-03-20 12:27:39 +0100

    Initial project setup

---
+++ Source/UE5_RPG/Public/Dummy.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "UObject/Object.h"
+#include "Dummy.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class UE5_RPG_API UDummy : public UObject
+{
+	GENERATED_BODY()
+	
+};
